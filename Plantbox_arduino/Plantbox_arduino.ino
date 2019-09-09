41bd0f1126e5ac0d1e6eaf566d0db93f0e670c9f
EricPwg <dz92286@gmail.com>
2019-09-09 15:59:40 +0800

    Add comment

--- Plantbox_arduino/Plantbox_arduino.ino
+++ Plantbox_arduino/Plantbox_arduino.ino
@@ -1,14 +1,10 @@
+#include <Bridge.h>
 #include <Adafruit_AM2320.h>
 #include <FastLED.h>
+
 #define NUM_LEDS 60
 #define LED_PIN1 5
 #define LED_PIN2 6
-
-#include <Bridge.h>
-#include "T6603.h"
-#include "DHT.h"
-
-#define DELAYTIME 2000
 
 #define DHT_Pin 9
 #define co2_TX_Pin 10
@@ -17,12 +13,17 @@
 #define water_Pin A1
 #define VRefer 5
 
+#include "T6603.h"
+#include "DHT.h"
+
+#define DELAYTIME 200
+#define WATERLEVEL_MIN 40
+#define WATERLEVEL_PEAK 10
+
 CRGB leds1[NUM_LEDS];
 CRGB leds2[NUM_LEDS];
 
-unsigned short watching_dog;
 int outcoming [13];
-
 char putvaletmp[100] = {'\0'};
 
 T6603 co2_sensor;
@@ -32,35 +33,30 @@ double get_o2(){
    int sum = 0;
    for(int i=0; i<8; i++) sum += analogRead(o2_Pin);
    sum >>= 3;
-
    double MeasuredVout = sum * (VRefer / 1024.0)*17.5;
    return MeasuredVout;
 }
 
 double get_waterlevel(){
   int value = analogRead(water_Pin);
-  if (value < 200) return 40;
-  if (value < 400) return 50;
-  if (value < 600) return 60;
-  if (value < 850) return 70;
-  return 80;
+  if (value < 200) return WATERLEVEL_MIN;
+  if (value < 400) return WATERLEVEL_MIN+WATERLEVEL_PEAK*1;
+  if (value < 600) return WATERLEVEL_MIN+WATERLEVEL_PEAK*2;
+  if (value < 850) return WATERLEVEL_MIN+WATERLEVEL_PEAK*3;
+  return WATERLEVEL_MIN+WATERLEVEL_PEAK*4;
 }
 
 void get_value(char *dataname, int pinNum, int mode = 0){
   char num[5] = {'\0'};
   Bridge.get(dataname, num, 5);
   num[4] = '\0';
-  //Serial.println(dataname);
-  //Serial.println(num);
   if (num[0] == '\0'){
-    //Serial.println("NAN");
     return ;
   }
   int val = atoi(num);
   if (mode == 0){
     if (val > 0){
       digitalWrite(pinNum, HIGH);
-      watching_dog |= (1 << pinNum);
     } 
     else digitalWrite(pinNum, LOW);
   }
@@ -81,36 +77,18 @@ void set_LED(){
   LEDR[5] = '\0';
   LEDG[5] = '\0';
   LEDB[5] = '\0';
-  Serial.println(LEDG);
-  Serial.println(LEDB);
-  Serial.println(LEDR);
-  //Serial.println(dataname);
-  //Serial.println(num);
-  int valR = atoi(LEDR);
-  int valG = atoi(LEDG);
-  int valB = atoi(LEDB);
+  
+  int valR = (LEDR[0] == '\0') ? 0 : atoi(LEDR);
+  int valG = (LEDG[0] == '\0') ? 0 : atoi(LEDG);
+  int valB = (LEDB[0] == '\0') ? 0 : atoi(LEDB);
+  valR = (valR > 255) ? 255 : (valR < 0) ? 0 : valR;
+  valG = (valG > 255) ? 255 : (valG < 0) ? 0 : valG;
+  valB = (valB > 255) ? 255 : (valB < 0) ? 0 : valB;
   
   for (int i=0; i<NUM_LEDS; i++) {
     leds1[i] = CRGB(valG, valR, valB);
     leds2[i] = CRGB(valG, valR, valB);
   }
-  Serial.println(valG);
-  Serial.println(valB);
-  Serial.println(valR);
-  /*
-  if (val2 == 1){
-    for (int i=0; i<NUM_LEDS; i+=2){
-      leds1[i] = CRGB(0, 200, 200);
-      leds2[i] = CRGB(0, 200, 200);
-    }
-  }
-  if (val3 == 1){
-    for (int i=1; i<NUM_LEDS; i+=2){
-      leds1[i] = CRGB(200, 200, 200);
-      leds2[i] = CRGB(200, 200, 200);
-    }
-  }
-  */
   FastLED.show();
 }
 
@@ -123,10 +101,7 @@ void put_value(char *dataname, double data, int index){
   outcoming[index] = outcoming[index]^1;
   char outcomedata[5];
   itoa(outcoming[index], outcomedata, 10);
-  //Serial.print(outcome);
-  //Serial.println(outcomedata);
   Bridge.put(outcome, outcomedata);
-  
 }
 
 void setup() {
@@ -134,13 +109,15 @@ void setup() {
     outcoming[i] = 0;
   }
 
+  //Digital output init.
   for (int i=2;i<8;i++){
     pinMode(i, OUTPUT);
   }
   for (int i=2;i<8;i++){
     digitalWrite(i, LOW);
   }
-
+  
+  //LED init.
   FastLED.addLeds<WS2813, LED_PIN1, RGB>(leds1, NUM_LEDS);
   FastLED.addLeds<WS2813, LED_PIN2, RGB>(leds2, NUM_LEDS);
   for (int i=0; i<NUM_LEDS; i++) {
@@ -149,7 +126,7 @@ void setup() {
   }
   FastLED.show();
   
-  watching_dog = 0;
+  //Sensor init.
   Bridge.begin();
   Serial.begin(9600);
   Serial.println("Setup");
@@ -158,75 +135,25 @@ void setup() {
 }
 
 void loop() {
-  
   //co2_sensor.begin(co2_TX_Pin, co2_RX_Pin);
   double humi = dht.readHumidity();
   double temp = dht.readTemperature();
   double co2 = (double)co2_sensor.get_co2();
   double o2 = get_o2();
   double waterlevel = get_waterlevel();
-
+  
+  //Get sensor value and put it to bridge
   put_value("Humidity-I", humi, 0);
   put_value("Temperature-I", temp, 1);
   put_value("CO2-I", co2, 2);
   put_value("O2-I", o2, 3);
   put_value("WaterLevel-I", waterlevel, 4);
-  //put_value("Watching Dog", watching_dog, 5);
-
-
-  watching_dog = 0;
+  
+  //Get signal from brigde.
   get_value("PumpWater-O", 2);
   get_value("Spray-O", 3);
   get_value("AddWater-O", 4);
-  //get_value("LED1", 5);
-  //get_value("LED2", 6);
-  //get_value("LED3", 7);
   set_LED();
-  watching_dog >>= 2;
-  //Serial.println(co2);
-  /*
-  for (int i=2;i<8;i++){
-    digitalWrite(i, HIGH);
-  }
-  delay(2000);
-  for (int i=2;i<8;i++){
-    digitalWrite(i, LOW);
-  }
-  Serial.println("123");
-  */
-  /*
-  char buf[10];
-  Bridge.get("CO2 Plantbox", buf, 10);
-  
-  Serial.print("CO2 = ");
-  buf[6] = '\0';
-  Serial.print(buf);
-  Serial.print("PPM\tO2 = ");
-  Bridge.get("O2 Plantbox", buf, 10);
-  buf[6] = '\0';
-  Serial.print(buf);
-  Serial.print("% Water_Level = ");
-  Bridge.get("Water Level Plantbox", buf, 10);
-  buf[6] = '\0';
-  Serial.print(buf);
-  Serial.print(" Temp = ");
-  Bridge.get("Temperature Plantbox", buf, 10);
-  buf[6] = '\0';
-  Serial.print(buf);
-  Serial.print(" Humidity = ");
-  Bridge.get("Humidity Plantbox", buf, 10);
-  buf[6] = '\0';
-  Serial.print(buf);
-  buf[6] = '\0';
-  Serial.println("\n");
-  Bridge.get("LED1 Plantbox", buf, 10);
-  buf[6] = '\0';
-  Serial.println(buf);
-  for (int i=0;i<13;i++){
-    Serial.print(outcoming[i]);
-  }
-  Serial.print("\n");*/
   
   delay(DELAYTIME);
-  //watching_dog++;
 }
